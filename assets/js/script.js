258e3db42c0c662acbdece6e368c6e496912cd05
Wellington Costa <fcowellcosta@gmail.com>
2021-02-07 10:35:32 -0300

    Criação do movimento da snake

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,55 @@ let context = canvas.getContext("2d");
 
 //informa quantos pixels terá nosso quadradinho
 let box=32;
+let snake =[];
+snake[0]={
+    x:8*box,
+    y:8*box
+}
+
+//criar variável que a cobrinha vai seguir
+let direction ="right";
+
+
+
+
 
 function criarBG() {
     context.fillStyle="lightgreen";
     context.fillRect(0,0,16*box,16*box);
-
 }
 
-criarBG();
+function criarCobrinha(){
+    for(i=0; i<snake.length; i++){
+        context.fillStyle="green";
+        context.fillRect(snake[i].x,snake[i].y, box, box);
+    }
+}
+
+
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX=snake[0].x;    
+    let snakeY=snake[0].y;
+
+    if(direction == "right") snakeX +=box;
+    if(direction == "left") snakeX +=box;
+
+    if(direction == "up") snakeY +=box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+
+    }
+    snake.unshift(newHead);
+
+}
+//Incia jogo com intervalo de 100 milisegundos para jogo não travar
+let jogo = setInterval(iniciarJogo,100);
+
